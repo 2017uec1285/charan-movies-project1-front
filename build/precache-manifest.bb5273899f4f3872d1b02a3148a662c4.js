94461e017108dad0cd1709a6eb99c3b21156ce1d
charan <2017uec1285@mnit.ac.in>
2020-08-15 11:34:17 +0530

    first commit1

---
+++ build/precache-manifest.bb5273899f4f3872d1b02a3148a662c4.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e45d2b5cea0c2c9f451d23640ce5a87",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bdfa6ad27273067b3f92",
+    "url": "/static/css/2.6c02c5f5.chunk.css"
+  },
+  {
+    "revision": "ff3a45dc6b8280823492",
+    "url": "/static/css/main.c481f62a.chunk.css"
+  },
+  {
+    "revision": "bdfa6ad27273067b3f92",
+    "url": "/static/js/2.7aa20826.chunk.js"
+  },
+  {
+    "revision": "2a34a1dc554c0f590228002c443969db",
+    "url": "/static/js/2.7aa20826.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff3a45dc6b8280823492",
+    "url": "/static/js/main.34d94e99.chunk.js"
+  },
+  {
+    "revision": "62e5aac5ebe50afc9fe6",
+    "url": "/static/js/runtime-main.0ee0ab35.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
